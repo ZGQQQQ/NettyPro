6eb53a83ba58e5d9187c9751e85ab7f279cd933d
zhuguangquan <zhuguangquan@enmonster.com>
2020-11-29 09:28:15 +0800

    netty编解码器注释

--- src/main/java/com/atguigu/netty/inboundhandlerandoutboundhandler/MyServerInitializer.java
+++ src/main/java/com/atguigu/netty/inboundhandlerandoutboundhandler/MyServerInitializer.java
@@ -6,11 +6,9 @@ import io.netty.channel.socket.SocketChannel;
 
 
 public class MyServerInitializer extends ChannelInitializer<SocketChannel> {
-
     @Override
     protected void initChannel(SocketChannel ch) throws Exception {
         ChannelPipeline pipeline = ch.pipeline();//一会下断点
-
         //入站的handler进行解码 MyByteToLongDecoder
         //pipeline.addLast(new MyByteToLongDecoder());
         pipeline.addLast(new MyByteToLongDecoder2());
